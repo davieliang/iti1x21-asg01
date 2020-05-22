8a7adc898efdf5d7a0c03b366f39e2706a981583
davieliang <65671773+davieliang@users.noreply.github.com>
2020-05-21 20:14:14 -0400

    Update CellValue.java

--- CellValue.java
+++ CellValue.java
@@ -9,7 +9,13 @@
  */
 
 public enum CellValue {
-
-	// YOUR CODE HERE
-
+List<List<Integer>> winCon = new ArrayList<List<Integer>>();
+		winCon.add(Arrays.asList(1, 2, 3)); 
+		winCon.add(Arrays.asList(4, 5, 6)); 
+		winCon.add(Arrays.asList(7, 8, 9)); 
+		winCon.add(Arrays.asList(1, 4, 7)); 
+		winCon.add(Arrays.asList(2, 5, 8)); 
+		winCon.add(Arrays.asList(3, 6, 9)); 
+		winCon.add(Arrays.asList(1, 5, 9)); 
+		winCon.add(Arrays.asList(3, 5, 7)); 
 }
