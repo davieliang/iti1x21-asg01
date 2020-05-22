ef5d9dcf12a456bc84cc104fbc35eda89abc07fb
davieliang <65671773+davieliang@users.noreply.github.com>
2020-05-21 20:41:16 -0400

    Update CellValue.java

--- CellValue.java
+++ CellValue.java
@@ -9,7 +9,65 @@
  */
 
 public enum CellValue {
+public static void placePiece(char[][] gameBoard, int placement, String user) {
+        char symbol;
+        char cpuIcon;
+        char userIcon;
+        if(userSymbol == 1) {
+            cpuIcon = 'O';
+            userIcon = 'X';
+        } else {
+            cpuIcon = 'X';
+            userIcon = 'O';
+        }
+        if(user.equals("cpu")) {
+            symbol = cpuIcon;
+            cpuPosition.add(placement);
+            System.out.println("\n Cpu placed at at " + placement);
 
-	// YOUR CODE HERE
+        } else {
+            symbol = userIcon;
+            playerPosition.add(placement);
+            System.out.println("\n" + user+ "Placed at " + placement);
 
-}
+        }
+        switch (placement) {
+
+        case 1:
+            gameBoard[0][0] = symbol;
+            break;
+        case 2:
+            gameBoard[0][2] = symbol;
+            break;
+        case 3:
+            gameBoard[0][4] = symbol;
+            break;
+        case 4:
+            gameBoard[2][0] = symbol;
+            break;
+        case 5:
+            gameBoard[2][2] = symbol;
+            break;
+        case 6:
+            gameBoard[2][4] = symbol;
+            break;
+        case 7:
+            gameBoard[4][0] = symbol;
+            break;
+        case 8:
+            gameBoard[4][2] = symbol;
+            break;
+        case 9:
+            gameBoard[4][4] = symbol;
+            break;
+        }
+
+    }
+    public static void printGameConsole(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
